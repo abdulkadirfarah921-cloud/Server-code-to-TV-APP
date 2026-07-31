7a3a9352991ab8d03761bd82654d48a98f78f89a
SHADOWKING MOBILE DEV... <abdulkadirfarah921@gmail.com>
2026-07-31 08:20:38 +0300

    Fix import path for User model in admin.js

--- admin.js
+++ admin.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
+const User = require('../User');
 const LicenseCode = require('../models/LicenseCode');
 const ServerLog = require('../models/ServerLog');
 const Channel = require('../models/Channel');
